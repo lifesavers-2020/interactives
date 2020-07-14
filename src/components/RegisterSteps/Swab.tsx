8200f65cd128b22ebd122956369dda781536ccaa
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-13 21:04:36 -0700

    Add swab action

--- src/components/RegisterSteps/Swab.tsx
+++ src/components/RegisterSteps/Swab.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { motion, useMotionValue } from "framer-motion";
 
 export const Swab: React.FC = () => {
-  return <img src="/assets/imgs/register-steps/swabbing.png" alt="Swabbing" />;
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  return (
+    <div>
+      <div>
+        <img src="/assets/imgs/register-steps/swab/bg.png" />
+      </div>
+      <div
+        className="relative"
+        style={{ zIndex: -1, width: 180, top: -180, left: 16 }}
+      >
+        <img src="/assets/imgs/register-steps/swab/head.png" />
+      </div>
+      <motion.div
+        drag
+        dragConstraints={{ top: -8, bottom: 5, left: -5, right: 5 }}
+        dragElastic={0.01}
+        dragMomentum={false}
+        style={{ x, y }}
+      >
+        <div className="relative" style={{ width: 153, top: -290, left: 0 }}>
+          <img
+            draggable={false}
+            src="/assets/imgs/register-steps/swab/hand.png"
+          />
+        </div>
+      </motion.div>
+    </div>
+  );
 };
