93b574d901d259321ee3aa11cfeb2a9a3554dcc7
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-15 22:57:10 -0700

    Add peripheral blood donation pics

--- src/pages/PeripheralBloodDonation.tsx
+++ src/pages/PeripheralBloodDonation.tsx
@@ -5,9 +5,6 @@ import { NextStepButton } from "../components/Share/NextStepButton";
 export const PeripheralBloodDonation: React.FC = () => {
   return (
     <VContainer>
-      PeripheralBloodDonation
-      <br />
-      WIP
       <NextStepButton to="/find-match-takes-time" />
     </VContainer>
   );
