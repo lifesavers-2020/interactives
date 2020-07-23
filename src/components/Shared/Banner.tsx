c0f9925862e9d7d566b0345fe1d62457716c78b4
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-23 16:05:18 -0700

    Style info box

--- src/components/Shared/Banner.tsx
+++ src/components/Shared/Banner.tsx
@@ -5,7 +5,12 @@ export const Banner: React.FC = () => {
   return (
     <div className="flex flex-row justify-around items-center w-full">
       <img width={width} src="/assets/imgs/shared/cbs-logo.png" alt="Logo" />
-      <a target="_blank" href="https://www.blood.ca/en" style={{ width }}>
+      <a
+        target="_blank"
+        href="https://www.blood.ca/en"
+        rel="noopener noreferrer"
+        style={{ width }}
+      >
         <button className="w-full">Register</button>
       </a>
     </div>
