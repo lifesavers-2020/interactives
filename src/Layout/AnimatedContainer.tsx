c62204ff6c267e9435ceb5f63c6f9cbce45efc06
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-22 22:15:08 -0700

    Update page transition effects

--- src/Layout/AnimatedContainer.tsx
+++ src/Layout/AnimatedContainer.tsx
@@ -5,9 +5,9 @@ export const AnimatedContainer: React.FC = ({ children }) => {
   return (
     <motion.div
       className="absolute flex flex-col justify-between h-full w-full"
-      initial={{ x: "-100vw", opacity: 0, scale: 0.8 }}
+      initial={{ x: "-100vw", opacity: 0, scale: 1 }}
       animate={{ x: 0, opacity: 1, scale: 1 }}
-      exit={{ x: "100vw", opacity: 0, scale: 0.8 }}
+      exit={{ x: 0, opacity: 0, scale: 0.5 }}
       transition={{ type: "tween", ease: "anticipate", duration: 0.5 }}
     >
       {children}
