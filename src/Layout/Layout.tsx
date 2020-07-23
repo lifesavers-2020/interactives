9206c9c966b6eff23d79f802c2daea1349e05767
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-23 15:01:26 -0700

    Add banner

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { NextStepButton } from "../components/Shared/NextStepButton";
 import { PageTransition } from "../components/Animations/PageTransition";
+import { Banner } from "../components/Shared/Banner";
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <PageTransition>
+      <div className="flex-none p-4">
+        <NextStepButton />
+      </div>
       <div className="flex-auto flex flex-col justify-around items-center h-full w-full">
         {children}
       </div>
       <div className="flex-none p-4">
-        <NextStepButton />
+        <Banner />
       </div>
     </PageTransition>
   );
---
+++ src/components/Shared/Banner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Banner: React.FC = () => {
+  const width = 120;
+  return (
+    <div className="flex flex-row justify-around items-center w-full">
+      <img width={width} src="/assets/imgs/shared/cbs-logo.png" alt="Logo" />
+      <a target="_blank" href="https://www.blood.ca/en" style={{ width }}>
+        <button className="w-full">Register</button>
+      </a>
+    </div>
+  );
+};
