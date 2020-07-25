6958a69e6fc57658b66a70e295adb1e96b530b69
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-24 21:18:43 -0700

    Set font and update logo

--- src/components/Layouts/Banner.tsx
+++ src/components/Layouts/Banner.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import { motion } from "framer-motion";
 
 export const Banner: React.FC = () => {
-  const width = 120;
+  const width = 140;
   return (
     <div className="flex flex-row justify-around items-center w-full">
-      <img width={width} src="/assets/imgs/shared/cbs-logo.png" alt="Logo" />
+      <img width={width} src="/assets/imgs/shared/cbs-logo.svg" alt="Logo" />
       <a
         target="_blank"
         href="https://www.blood.ca/en"
         rel="noopener noreferrer"
-        style={{ width }}
       >
         <motion.button
-          className="w-full register-button"
+          className="w-32 register-button"
           whileTap={{ scale: 0.9 }}
         >
           Register Now
