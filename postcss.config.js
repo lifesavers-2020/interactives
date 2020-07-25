3eb468e1afdc510005ad8376d7dd352c384af739
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-25 03:08:15 -0700

    Purge tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+    "./public/index.html",
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
