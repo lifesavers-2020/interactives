1756275356d0263e787f29e748237cede37da3e2
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-29 17:01:31 -0700

    Move UI to higher z index

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -7,19 +7,19 @@ import { ProgressOverview } from "./ProgressOverview";
 export const Layout: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col justify-between w-full h-full overflow-hidden relative">
-      <div className="flex-none p-4">
+      <div className="flex-none p-4 z-10 relative">
         <ProgressOverview />
       </div>
-      <div className="flex-none flex flex-row justify-center">
+      <div className="flex-none flex flex-row justify-center z-10 relative">
         <Divider />
       </div>
       <div className="relative flex-auto flex flex-col justify-around items-center h-full w-full">
         <PageTransition>{children}</PageTransition>
       </div>
-      <div className="flex-none flex flex-row justify-center py-1">
+      <div className="flex-none flex flex-row justify-center py-1 z-10 relative">
         <Divider />
       </div>
-      <div className="flex-none p-4 bg-white">
+      <div className="flex-none p-4 bg-white z-10 relative">
         <Banner />
       </div>
     </div>
