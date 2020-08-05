0c448616aff1923020f32692478b33554a7646f6
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-08-04 23:26:52 -0700

    Fix autoplay audio

--- src/components/Shared/AudioManager.tsx
+++ src/components/Shared/AudioManager.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const bgm = require("../../assets/audio/bgm.ogg");
 
 const audio = new Audio(bgm);
 
 export const AudioManager: React.FC = () => {
-  audio.loop = true;
-  audio.play();
+  useEffect(() => {
+    document.body.onfocus = () => {
+      audio.loop = true;
+      audio.play();
+    };
+    document.body.onblur = () => {
+      audio.pause();
+    };
+  }, []);
+
   return <></>;
 };
